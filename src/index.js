63d050edb7a7a759365e7954c1d84bfdb1f7cf4c
Grace Durant <gdurant34@gmail.com>
2022-09-29 09:48:28 -0700

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-// Your code here
+const URL = 'http://localhost:3000/characters';
+const form = document.getElementById('votes-form');
+form.addEventListener('submit', newVoteForm)
+
+fetch(URL)
+    .then(res => res.json())
+    .then(renderChars)
+
+function renderChars(chars) {
+    chars.forEach(renderChar);
+}
+
+function renderChar(char) {
+    const CharBarDiv = document.getElementById('character-bar');
+    const charNameSpan = document.createElement('span');
+    charNameSpan.textContent = char.name;
+    CharBarDiv.append(charNameSpan);
+    
+    charNameSpan.addEventListener('click', e => renderDetails(char));
+}
+
+function renderDetails(char) {
+    const nameDisplay = document.getElementById('name');
+    const imgDisplay = document.getElementById('image');
+    const voteDisplay = document.getElementById('vote-count');
+
+    nameDisplay.textContent = char.name;
+    imgDisplay.src = char.image;
+    voteDisplay.textContent = char.votes;
+}
+
+function newVoteForm(e) {
+    e.preventDefault();
+    const votes = e.target.votes.value;
+    const voteDisplay = document.getElementById('vote-count');
+    voteDisplay.textContent = parseInt(voteDisplay.textContent) + parseInt(votes);
+    form.reset();
+}
